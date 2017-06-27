a713cf3815f197832c86d5a270286e204db59a97
joshinrai <joshinrai@163.com>
2017-06-27 16:58:58 +0800

    修改
    修改

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app'
+  }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-}
-img{
-  position:absolute;
-  height:100vh;
-  width:100vw;
-  animation-name: animar_fondo;
-  animation-duration: 20s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-  animation-direction: alternate;
-}
-@-webkit-keyframes animar_fondo {
-  from { -webkit-transform: scale(1) translate(0px);
--moz-transform: scale(1) translate(0px);
--ms-transform: scale(1) translate(0px);
--o-transform: scale(1) translate(0px);
-transform: scale(1) translate(0px); }
-  to { -webkit-transform: scale(1.5) translate(50px);
--moz-transform: scale(1.5) translate(50px);
--ms-transform: scale(1.5) translate(50px);
--o-transform: scale(1.5) translate(50px);
-transform: scale(1.5) translate(50px); }
 }
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,90 @@
 <template>
   <div class="login">
-    <h1>{{ msg }}</h1>
+  	<img src="../assets/wap.jpg">
+  	<div class="login_index">
+  		<h1>{{ msg }}</h1>
+  		<p>{{ cont }}</p>
+  		<button>继续</button>
+  	</div>
+	<div class="login_cont">
+		<h2>正泰燃气表综合管理平台</h2>
+		<input type="text" placeholder="用户名">
+		<input type="text" placeholder="密码">
+		<button @click="test">登录</button>
+	</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'login',
-  data () {
-    return {
-      msg: '正泰燃气综合管理平台'
-    }
-  }
-}
+	export default {
+	  name: 'login',
+	  data () {
+	    return {
+	      msg: '正泰燃气综合管理平台',
+	      cont: '为用户提供营收管理、抄表、决策辅助分析和公共增值服务功能。系统支持基表、普通IC卡表、微功率无线表、物联网表，并保证系统正常稳定运行。'
+	    }
+	  }
+	}
 </script>
 
 <!-- 添加 "scoped" 属性用于限制样式只作用域当前组件 -->
 <style scoped>
-h1 {
+.login_index{
+	position: absolute;
+    background-color: rgba(187, 168, 170, 0.79);
+    width: 80vw;
+	height:30vh;
+    border-radius:3px ;
+    margin-top: 25vh;
+    margin-left:10vw;
+    padding: 15px 0px;
+}
+h1,p {
   font-weight: normal;
+  text-align:center;
+  font-size:2.5em;
+  margin-top:1em;
+  color:#fff;
+}
+button{
+	background-color: #FF9800;
+  	border: none;
+    margin-left: 25%;
+    width: 50%;
+    border-radius:5px;
+    box-shadow: 1px 5px 20px -5px rgba(0,0,0,0.4);
+    color: #fff;
+    margin-top: 1.5em;
+    cursor: pointer;
+    font-size:3em;
+    line-height:2.5em;
+}
+img{
+  position:absolute;
+  height:100vh;
+  width:100vw;
+  opacity:0.5;
+  animation-name: animar_fondo;
+  animation-duration: 20s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+@media (max-width: 460px){
+	h1{
+		font-size:4em;
+	}
+}
+@-webkit-keyframes animar_fondo {
+  from { -webkit-transform: scale(1) translate(0px);
+-moz-transform: scale(1) translate(0px);
+-ms-transform: scale(1) translate(0px);
+-o-transform: scale(1) translate(0px);
+transform: scale(1) translate(0px); }
+  to { -webkit-transform: scale(1.5) translate(50px);
+-moz-transform: scale(1.5) translate(50px);
+-ms-transform: scale(1.5) translate(50px);
+-o-transform: scale(1.5) translate(50px);
+transform: scale(1.5) translate(50px); }
 }
 </style>
